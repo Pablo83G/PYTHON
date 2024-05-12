e963580b136ac2fab1df3b0f7bc515d74156aa29
Pablo83G <128129967+Pablo83G@users.noreply.github.com>
2024-05-12 19:44:46 +0200

    Delete ejercicios_py/p directory

--- ejercicios_py/p/pruebas2.py
+++
@@ -1,9 +0,0 @@
-def imprime_lista_de_letras(letras, palabra):
-  # letras = set(letras)
-  palabra = palabra.upper()
-  return ' '.join(['_' if l in letras else l for l in palabra])
-
-def esta_palabra_completa(letras, palabra):
-  for l in palabra:
-    if l not in letras:
-      return False
